587d55cb80357232864fe6521f4ebbe8eb77626a
vikaspatil2277 <vikaspatil2277@gmail.com>
2023-09-27 23:43:04 +0530

    modified

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
 const Footer = () => {
   return (
     <div className='footer'>
        <div className='header-content'>&copy; 2023 Your Company. All rights reserved.</div>
-  <div className='header-content'>Email: <a href="mailto:vikaspatil2277@gmail.com" className='mail'>vikaspatil2277@gmail.com</a></div>
+  <div className='header-content' > <a  href="mailto:vikaspatil2277@gmail.com" className='mail'>vikaspatil2277@gmail.com </a></div>
     </div>
     
   )
